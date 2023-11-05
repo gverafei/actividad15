5b7744bbfc2665296d23bc05f9b5e73d4c433667
gverafei <gvera@uv.mx>
2023-11-05 11:27:17 -0600

    Add project files.

---
+++ CorreoFei/Program.cs
@@ -0,0 +1,38 @@
+using CorreoFei.Services.Email;
+using CorreoFei.Services.ErrorLog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Mis servicios
+
+// Soporte para guardar los errores
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<IErrorLog, ErrorLog>();
+// Soporte para enviar correo
+builder.Services.AddScoped<IEmail, Email>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ CorreoFei/Services/ErrorLog/ErrorLog.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace CorreoFei.Services.ErrorLog
+{
+    public class ErrorLog : IErrorLog
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ErrorLog(IWebHostEnvironment webHostEnvironment, IHttpContextAccessor httpContextAccessor)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpPost]
+        public async Task ErrorLogAsync(string Mensaje)
+        {
+            try
+            {
+                string webRootPath = _webHostEnvironment.WebRootPath;
+
+                string path = "";
+                path = Path.Combine(webRootPath, "log");
+
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                // Retreive server/local IP address
+                var feature = _httpContextAccessor.HttpContext.Features.Get<IHttpConnectionFeature>();
+                string LocalIPAddr = feature?.LocalIpAddress?.ToString();
+
+                // Write the specified text asynchronously to a new file named "WriteTextAsync.txt".
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, "log.txt"), true))
+                {
+                    await outputFile.WriteLineAsync(Mensaje + " - " + _httpContextAccessor.HttpContext.User.Identity.Name + " - " + LocalIPAddr + " - " + DateTime.Now.ToString());
+                }
+            }
+            catch
+            {
+                //No hace nada
+            }
+        }
+    }
+}
